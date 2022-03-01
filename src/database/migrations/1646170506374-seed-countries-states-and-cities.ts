95721e4a67ea948dcf93ef27389c5b28817b793f
Daniel Cruz <danielcunha54@gmail.com>
2022-03-01 18:40:58 -0300

    chore: seed countries, states and cities

---
+++ src/database/migrations/1646170506374-seed-countries-states-and-cities.ts
@@ -0,0 +1,39 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+import countries from '../data/countries.json'
+import { Country, State } from '../entities'
+export class seedCountriesStatesAndCities1646170506374 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    let values = countries.map(({ name, alpha2 }) => `('${name}', '${alpha2}')`).join(', ')
+    const results: Country[] = await queryRunner.query(
+      `INSERT INTO countries (name, code) VALUES ${values} RETURNING *`
+    )
+
+    for (const country of countries) {
+      const countryId = results.find(({ code }) => code === country.alpha2)!.id
+      values = country.states
+        .map(({ name, abbreviation }) => `('${countryId}', '${name}', '${abbreviation}')`)
+        .join(', ')
+      const statesResults: State[] = await queryRunner.query(
+        `INSERT INTO states (country_id, name, code) VALUES ${values} RETURNING *`
+      )
+
+      for (const state of country.states) {
+        const stateId = statesResults.find(({ code }) => code === state.abbreviation)!.id
+        values = state.cities
+          .map(name => {
+            const sanitizedName = name.replace("'", "''")
+            return `('${stateId}', '${sanitizedName}')`
+          })
+          .join(', ')
+        await queryRunner.query(`INSERT INTO cities (state_id, name) VALUES ${values}`)
+      }
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    for (const { alpha2 } of countries) {
+      await queryRunner.query(`DELETE FROM countries WHERE code = $1`, [alpha2])
+    }
+  }
+}
