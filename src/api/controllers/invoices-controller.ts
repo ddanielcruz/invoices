fe3b2b60467c595f6c6faac908bc02d004281be1
Daniel Cruz <danielcunha54@gmail.com>
2022-03-20 19:25:33 -0300

    feat: create endpoint to find invoices by period

--- src/api/controllers/invoices-controller.ts
+++ src/api/controllers/invoices-controller.ts
@@ -1,7 +1,15 @@
 import { Request, Response } from 'express'
 import { container } from 'tsyringe'
 
+import { FindManyByPeriod } from '../../core/services/invoices/find-many-by-period'
 import { QueueInvoiceExtraction } from '../../core/services/invoices/queue-invoice-extraction'
+
+export async function listByPeriod(request: Request, response: Response): Promise<Response> {
+  const service = container.resolve(FindManyByPeriod)
+  const invoices = await service.execute(request.query as any)
+
+  return response.json(invoices)
+}
 
 export async function create(request: Request, response: Response): Promise<Response> {
   const service = container.resolve(QueueInvoiceExtraction)
--- src/api/routes/invoices-routes.ts
+++ src/api/routes/invoices-routes.ts
@@ -3,4 +3,5 @@ import { Router } from 'express'
 import * as invoices from '../controllers/invoices-controller'
 
 export const routes = Router()
+routes.get('/', invoices.listByPeriod)
 routes.post('/', invoices.create)
